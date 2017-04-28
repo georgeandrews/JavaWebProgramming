9e04301f3587b5d02fd28c94714bad1a79a3c1db
George Andrews <gandrews7@cvtc.edu>
2017-04-28 15:30:22 -0500

    Fix indentation.

--- MavenExample/src/main/java/com/iteachcoding/web/dao/impl/UserIO.java
+++ MavenExample/src/main/java/com/iteachcoding/web/dao/impl/UserIO.java
@@ -20,72 +20,72 @@ import com.iteachcoding.web.model.User;
  */
 public class UserIO {
 
-    public static boolean add(final User user, final String filepath) {
-        try {
-            final File file = new File(filepath);
-            final PrintWriter out = new PrintWriter(new FileWriter(file, true));
-            out.println(user.getEmail() + "|"
-                    + user.getFirstName() + "|"
-                    + user.getLastName());
+  public static boolean add(final User user, final String filepath) {
+    try {
+      final File file = new File(filepath);
+      final PrintWriter out = new PrintWriter(new FileWriter(file, true));
+      out.println(user.getEmail() + "|"
+          + user.getFirstName() + "|"
+          + user.getLastName());
 
-            out.close();
-            return true;
-        } catch (final IOException e) {
-            e.printStackTrace();
-            return false;
-        }
+      out.close();
+      return true;
+    } catch (final IOException e) {
+      e.printStackTrace();
+      return false;
     }
+  }
 
-    public static User getUser(final String email, final String filepath) {
-        try {
-            final BufferedReader reader = new BufferedReader(new FileReader(filepath));
-            User user = null;
-            String line = reader.readLine();
-            while (line != null) {
-              user = new User();
-              
-                final StringTokenizer tokenizer = new StringTokenizer(line, "|");
-                if (tokenizer.countTokens() < 3) {
-                    return null;
-                }
-                final String token = tokenizer.nextToken();
-                if (token.equalsIgnoreCase(email)) {
-                    final String firstName = tokenizer.nextToken();
-                    final String lastName = tokenizer.nextToken();
-                    user.setEmail(email);
-                    user.setFirstName(firstName);
-                    user.setLastName(lastName);
-                }
-                line = reader.readLine();
-            }
-            reader.close();
-            return user;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
+  public static User getUser(final String email, final String filepath) {
+    try {
+      final BufferedReader reader = new BufferedReader(new FileReader(filepath));
+      User user = null;
+      String line = reader.readLine();
+      while (line != null) {
+        user = new User();
 
-    public static ArrayList<User> getUsers(final String filepath) {
-        try {
-            final ArrayList<User> users = new ArrayList<User>();
-            final BufferedReader reader = new BufferedReader(new FileReader(filepath));
-            
-            String line = reader.readLine();
-            while (line != null) {
-                final StringTokenizer tokenizer = new StringTokenizer(line, "|");
-                final String email = tokenizer.nextToken();
-                final String firstName = tokenizer.nextToken();
-                final String lastName = tokenizer.nextToken();
-                final User user = new User(firstName, lastName, email);
-                users.add(user);
-                line = reader.readLine();
-            }
-            reader.close();
-            return users;
-        } catch (IOException e) {
-            System.out.println(e);
-            return null;
+        final StringTokenizer tokenizer = new StringTokenizer(line, "|");
+        if (tokenizer.countTokens() < 3) {
+          return null;
         }
+        final String token = tokenizer.nextToken();
+        if (token.equalsIgnoreCase(email)) {
+          final String firstName = tokenizer.nextToken();
+          final String lastName = tokenizer.nextToken();
+          user.setEmail(email);
+          user.setFirstName(firstName);
+          user.setLastName(lastName);
+        }
+        line = reader.readLine();
+      }
+      reader.close();
+      return user;
+    } catch (IOException e) {
+      e.printStackTrace();
+      return null;
     }
+  }
+
+  public static ArrayList<User> getUsers(final String filepath) {
+    try {
+      final ArrayList<User> users = new ArrayList<User>();
+      final BufferedReader reader = new BufferedReader(new FileReader(filepath));
+
+      String line = reader.readLine();
+      while (line != null) {
+        final StringTokenizer tokenizer = new StringTokenizer(line, "|");
+        final String email = tokenizer.nextToken();
+        final String firstName = tokenizer.nextToken();
+        final String lastName = tokenizer.nextToken();
+        final User user = new User(firstName, lastName, email);
+        users.add(user);
+        line = reader.readLine();
+      }
+      reader.close();
+      return users;
+    } catch (IOException e) {
+      System.out.println(e);
+      return null;
+    }
+  }
 }
--- MavenExample/src/main/java/com/iteachcoding/web/security/RegisterFilter.java
+++ MavenExample/src/main/java/com/iteachcoding/web/security/RegisterFilter.java
@@ -23,9 +23,9 @@ import com.iteachcoding.web.util.CookieUtility;
  */
 @WebFilter("/RegisterFilter")
 public class RegisterFilter implements Filter {
-  
+
   private FilterConfig filterConfig = null;
-  
+
   /**
    * @see Filter#init(FilterConfig)
    */
@@ -33,50 +33,50 @@ public class RegisterFilter implements Filter {
     this.filterConfig = fConfig;
   }
 
-	/**
-	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
-	 */
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-	  
-	  if (isIgnoredPath(request, filterConfig) || isRegistered(request, response)) {
-	    // pass the request along the filter chain
-	    chain.doFilter(request, response);
-	  } else {
-	    ((HttpServletRequest) request).getRequestDispatcher("register.jsp").forward(request, response);
-	  }
+  /**
+   * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+   */
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
-	}
-	
-	/**
-	 * Check the requestURI to see if it contains an ignored path using class constant and web.xml 
-	 * init parameter.  
-	 * 
-	 * @param requestURI
-	 * @param filterConfig
-	 * @return true if path should be ignored
-	 */
-	private static boolean isIgnoredPath(final ServletRequest request, final FilterConfig filterConfig) {
-	  
-	  final String requestURI = ((HttpServletRequest) request).getRequestURI();
-	  final String pathsToIgnore = filterConfig.getInitParameter("pathsToIgnore");
-	  
-	  for (final String path : pathsToIgnore.split(",")) {
-	    if (requestURI.contains(path)) {
-	      return true;
-	    }
-	  }
-	  return false;
-	}
-	
-	/**
-	 * Check to see if the user is active in session. 
-	 * If not, check for a cookie. Otherwise, return false 
-	 * so we can send user to registration page.
-	 * 
-	 * @param request
-	 * @param response
-	 * @return true if user is registered
-	 */
+    if (isIgnoredPath(request, filterConfig) || isRegistered(request, response)) {
+      // pass the request along the filter chain
+      chain.doFilter(request, response);
+    } else {
+      ((HttpServletRequest) request).getRequestDispatcher("register.jsp").forward(request, response);
+    }
+
+  }
+
+  /**
+   * Check the requestURI to see if it contains an ignored path using class constant and web.xml 
+   * init parameter.  
+   * 
+   * @param requestURI
+   * @param filterConfig
+   * @return true if path should be ignored
+   */
+  private static boolean isIgnoredPath(final ServletRequest request, final FilterConfig filterConfig) {
+
+    final String requestURI = ((HttpServletRequest) request).getRequestURI();
+    final String pathsToIgnore = filterConfig.getInitParameter("pathsToIgnore");
+
+    for (final String path : pathsToIgnore.split(",")) {
+      if (requestURI.contains(path)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Check to see if the user is active in session. 
+   * If not, check for a cookie. Otherwise, return false 
+   * so we can send user to registration page.
+   * 
+   * @param request
+   * @param response
+   * @return true if user is registered
+   */
   private static boolean isRegistered(final ServletRequest request, final ServletResponse response) {
 
     final HttpSession session = ((HttpServletRequest) request).getSession();
@@ -86,7 +86,7 @@ public class RegisterFilter implements Filter {
     if (user == null) {
       final Cookie[] cookies = ((HttpServletRequest) request).getCookies();
       final String emailAddress = CookieUtility.getCookieValue(cookies, "emailCookie");
-      
+
       // if cookie doesn't exist, go to registration page
       if (Strings.isNullOrEmpty(emailAddress)) {
         return false;
